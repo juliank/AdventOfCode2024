956c6a03812b0e222b9c35827040f75b74f4fd78
Julian Klock <julian.klock@gmail.com>
2024-12-18 23:39:27 +0100

    Solve puzzle 17 part 1
    All tests pass for part 2, but the solution is too slow for the real input.
    
    commit 3fd8636e44b718b6522f49e79c8174aad6d34ee5
    Author: Julian Klock <julian.klock@gmail.com>
    Date:   Wed Dec 18 23:38:37 2024 +0100
    
        Readme
    
    commit 228cde9cd4d8bee024a8ab799067a8f7d03a9617
    Merge: b47ff0f 0f1591b
    Author: Julian Klock <julian.klock@gmail.com>
    Date:   Wed Dec 18 23:35:07 2024 +0100
    
        Merge branch 'main' into wip/17
    
    commit b47ff0f14b0a1fd2920aeab19ac2f1ff84837331
    Author: Julian Klock <julian.klock@gmail.com>
    Date:   Wed Dec 18 23:33:55 2024 +0100
    
        Tweaks
    
    commit 3bdee895d730f6cbe411796f5ba35b76426a869e
    Author: Julian Klock <julian.klock@gmail.com>
    Date:   Tue Dec 17 22:27:00 2024 +0100
    
        Attempting to optimize by caching result, but execution loops in jnz
    
    commit 34402c54f58a6039bf240eec6107a09e288b1e95
    Author: Julian Klock <julian.klock@gmail.com>
    Date:   Tue Dec 17 22:17:58 2024 +0100
    
        Part 2 works on example, but is too slow on real input
    
    commit 113ace7de363ab9743aebd6b141366003676f2a5
    Author: Julian Klock <julian.klock@gmail.com>
    Date:   Tue Dec 17 21:51:25 2024 +0100
    
        Solve part 1
    
    commit 44a1508e15eccdb969bff066f1898b50c03120ab
    Author: Julian Klock <julian.klock@gmail.com>
    Date:   Tue Dec 17 08:36:58 2024 +0100
    
        Fixed bug in jnz, tests pass but result is unchanged (and still invalid)
    
    commit 51d511d62fa1f363694568f25124fee9297eb139
    Author: Julian Klock <julian.klock@gmail.com>
    Date:   Tue Dec 17 08:27:47 2024 +0100
    
        Part 1 works on all example input
    
    commit 0ef4aaaf975cb18701014eefe8b0533fafc0bbd7
    Author: Julian Klock <julian.klock@gmail.com>
    Date:   Tue Dec 17 08:14:44 2024 +0100
    
        Part 1 works no examples

--- AdventOfCode.Tests/Puzzles/Puzzle17Tests.cs
+++ AdventOfCode.Tests/Puzzles/Puzzle17Tests.cs
@@ -2,24 +2,110 @@ namespace AdventOfCode.Tests.Puzzles;
 
 public class Puzzle17Tests
 {
-    private readonly Puzzle17 _puzzle;
+    private Puzzle17 _puzzle;
 
     public Puzzle17Tests()
     {
         _puzzle = new Puzzle17();
     }
 
-    // [Fact(Skip = "Not yet implemented")]
+    [Fact]
     public void SolvePart1()
     {
         var result = _puzzle.SolvePart1();
-        result.Should().Be("");
+        result.Should().Be("4,6,1,4,2,1,3,1,6");
     }
 
-    // [Fact(Skip = "Not yet implemented")]
+    [Fact(Skip = "Not yet implemented")]
     public void SolvePart2()
     {
         var result = _puzzle.SolvePart2();
         result.Should().Be("");
+    }
+
+    [Fact]
+    public void TestPart1Example1()
+    {
+        _puzzle = new Puzzle17(
+            "Register A: 0",
+            "Register B: 0",
+            "Register C: 9",
+            "",
+            "Program: 2,6"
+        );
+        _ = _puzzle.SolvePart1();
+        _puzzle.B.Should().Be(1);
+    }
+
+    [Fact]
+    public void TestPart1Example2()
+    {
+        _puzzle = new Puzzle17(
+            "Register A: 10",
+            "Register B: 0",
+            "Register C: 0",
+            "",
+            "Program: 5,0,5,1,5,4"
+        );
+        var result = _puzzle.SolvePart1();
+        result.Should().Be("0,1,2");
+    }
+
+    [Fact]
+    public void TestPart1Example3()
+    {
+        _puzzle = new Puzzle17(
+            "Register A: 2024",
+            "Register B: 0",
+            "Register C: 0",
+            "",
+            "Program: 0,1,5,4,3,0"
+        );
+        var result = _puzzle.SolvePart1();
+        result.Should().Be("4,2,5,6,7,7,7,7,3,1,0");
+        _puzzle.A.Should().Be(0);
+    }
+
+    [Fact]
+    public void TestPart1Example4()
+    {
+        _puzzle = new Puzzle17(
+            "Register A: 0",
+            "Register B: 29",
+            "Register C: 0",
+            "",
+            "Program: 1,7"
+        );
+        _ = _puzzle.SolvePart1();
+        _puzzle.B.Should().Be(26);
+    }
+
+    [Fact]
+    public void TestPart1Example5()
+    {
+        _puzzle = new Puzzle17(
+            "Register A: 0",
+            "Register B: 2024",
+            "Register C: 43690",
+            "",
+            "Program: 4,0"
+        );
+        _ = _puzzle.SolvePart1();
+        _puzzle.B.Should().Be(44354);
+    }
+
+    [Fact]
+    public void TestPart2Example1()
+    {
+        _puzzle = new Puzzle17(
+            "Register A: 2024",
+            "Register B: 0",
+            "Register C: 0",
+            "",
+            "Program: 0,3,5,4,3,0"
+        );
+        _puzzle.RunningFromTests = true;
+        var result = _puzzle.SolvePart2();
+        result.Should().Be("117440");
     }
 }
